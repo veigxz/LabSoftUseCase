3aa6ad973a1aa5625f70fc662927341ce3abb53f
Manha Cetafaju <Manha.Cetafaju@labfies.org.br>
2026-08-28 10:48:05 -0300

    feat(usabilidade): adiciona versao 1.0.0 no titulo

--- SLN_Dias/AppDias/Program.cs
+++ SLN_Dias/AppDias/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("=================================");
-        Console.WriteLine("   SISTEMA DE GESTÃO DE TAREFAS  ");
+        Console.WriteLine(" SISTEMA DE GESTÃO DE TAREFAS - Versão 1.0.0");
         Console.WriteLine("=================================\n");
 
         Console.Write("Digite o nome da tarefa: ");
